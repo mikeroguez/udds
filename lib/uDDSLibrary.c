736689cca0535ec5469f52cf9ba944b1cb4bb110
mikeroguez <mike.roguez@gmail.com>
2011-02-28 11:23:59 -0600

    Con atof y atol... falta isspace isdigit

--- lib/uDDSLibrary.c
+++ lib/uDDSLibrary.c
@@ -167,6 +167,64 @@ done:
     return value * sign;
 }
 
+double atof(char *s)
+{
+        double a = 0.0;
+        int e = 0;
+        int c;
+        while ((c = *s++) != '\0' && isdigit(c)) {
+                a = a*10.0 + (c - '0');
+        }
+        if (c == '.') {
+                while ((c = *s++) != '\0' && isdigit(c)) {
+                        a = a*10.0 + (c - '0');
+                        e = e-1;
+                }
+        }
+        if (c == 'e' || c == 'E') {
+                int sign = 1;
+                int i = 0;
+                c = *s++;
+                if (c == '+')
+                        c = *s++;
+                else if (c == '-') {
+                        c = *s++;
+                        sign = -1;
+                }
+                while (isdigit(c)) {
+                        i = i*10 + (c - '0');
+                        c = *s++;
+                }
+                e += i*sign;
+        }
+        while (e > 0) {
+                a *= 10.0;
+                e--;
+        }
+        while (e < 0) {
+                a *= 0.1;
+                e++;
+        }
+        return a;
+}
+
+long atol(register const char *nptr)
+{
+	long total = 0;
+	int minus = 0;
+
+	while (isspace(*nptr)) nptr++;
+	if (*nptr == '+') nptr++;
+	else if (*nptr == '-') {
+		minus = 1;
+		nptr++;
+	}
+	while (isdigit(*nptr)) {
+		total *= 10;
+		total += (*nptr++ - '0');
+	}
+	return minus ? -total : total;
+}
 
 //end atod implementation   ******************************** modificado 161209 ***************************************************************************
 void showDataTopic(uDDS_MsgPackage xxxPackMsg){
